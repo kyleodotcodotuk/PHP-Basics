b1c39007e5b74271272c8c6108532ddcac0d9623
Kyle O'Connor <info@kyleo.co.uk>
2022-08-03 16:14:41 +0100

    Update video.php

--- components/video.php
+++ components/video.php
@@ -1,11 +1,11 @@
 <div class="wrapper">
 <?php 
-$d = dir("./video"); 
-echo "<ul>";
-while (false !== ($entry = $d->read())) {
-   echo "<li><a href='{$entry}'>{$entry}</a></li>";
-}
-echo "</ul>";
-$d->close();
+// $d = dir("./video"); 
+// echo "<ul>";
+// while (false !== ($entry = $d->read())) {
+//    echo "<li><a href='{$entry}'>{$entry}</a></li>";
+// }
+// echo "</ul>";
+// $d->close();
 ?>
 </div>
