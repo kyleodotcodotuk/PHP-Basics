efc3b92d9ac34a12f35a460cb62ab2444cad9426
Kyle O'Connor <info@kyleo.co.uk>
2022-08-04 13:20:33 +0100

    Video feed

--- components/video.php
+++ components/video.php
@@ -1,16 +1,23 @@
 <section class="video-feed">
-<div class="wrapper">
+<div class="wrapper flow flow-space-10">
 
-    <h2>Click on the links below to view</h2>
+    <h2>View videos below</h2>
 
-<?php 
-$d = dir("./video"); 
-echo "<ul>";
-while (false !== ($entry = $d->read())) {
-   echo "<li><a target='_blank' href='{$entry}'>{$entry}</a></li>";
-}
-echo "</ul>";
-$d->close();
-?>
+
+    <div class="grid gap-6">
+
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/CbMNISTzw20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/UA1pN7avKgw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/A7Y8ctDh55w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Kxgb9NcVKIw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/42KQYp2dgYU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                </div>
+
 </div>
 </section>
+
