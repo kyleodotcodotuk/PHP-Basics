10524c2b0329dbde81ffd9d919c92f092bf322d0
Kyle <Kyle.OConnor@brightstarr.com>
2024-05-22 16:20:25 +0100

    s

--- index.php
+++ index.php
@@ -2,6 +2,32 @@
 include('values/site.php');
  
 include('components/header.php'); 
+ 
+function get_image_urls($folder) {
+  $images = array();
+  if (is_dir($folder)) {
+    $scandir = scandir($folder);
+    foreach ($scandir as $item) {
+      if (in_array($item, array('.', '..'))) continue;
+      $path = $item; // Use only the filename within the folder
+      if (is_file($path) && getimagesize($path) !== false) {
+        $images[] = array(
+          'url' => $path,
+          'alt' => pathinfo($path, PATHINFO_FILENAME)
+        );
+      }
+    }
+  }
+  return $images;
+}
+
+$images = get_image_urls('src/assets/images'); // Assuming 'src/assets/images' is relative to images.php
+
+if ( !empty($images) ) :
+  foreach ($images as $image) : ?>
+    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+  <?php endforeach;
+endif; 
 
 include('components/video.php'); 
 
