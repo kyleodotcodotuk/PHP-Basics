dce4496039c6b7c2f25f23451676635f52de6457
Kyle <Kyle.OConnor@brightstarr.com>
2024-05-22 16:11:19 +0100

    s

--- components/video.php
+++ components/video.php
@@ -3,7 +3,37 @@
 
     <div class="grid gap-6">
  
-      <?php include 'images.php'; ?>
+    <h2>Images please</h2>
+
+    <?php
+
+    function get_image_urls($folder) {
+      $images = array();
+      if (is_dir($folder)) {
+        $scandir = scandir($folder);
+        foreach ($scandir as $item) {
+          if (in_array($item, array('.', '..'))) continue;
+          $path = $item; // Use only the filename within the folder
+          if (is_file($path) && getimagesize($path) !== false) {
+            $images[] = array(
+              'url' => $path,
+              'alt' => pathinfo($path, PATHINFO_FILENAME)
+            );
+          }
+        }
+      }
+      return $images;
+    }
+
+    $images = get_image_urls('src/assets/images'); // Assuming 'src/assets/images' is relative to images.php
+    
+    if ( !empty($images) ) :
+      foreach ($images as $image) : ?>
+        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+      <?php endforeach;
+    endif;
+    ?>
+
 
       <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/CbMNISTzw20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
